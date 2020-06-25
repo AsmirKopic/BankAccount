bae4d80a9db3ae663b838f2397662b3cdad68288
Asmir Kopic <asmir.kopic123@gmail.com>
2020-06-25 23:54:20 +0200

    BankAcc first commit

---
+++ Bank Account/src/BankAccount.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+		
+		
+		
+		try {
+			
+		// 1. Get connection to data base
+			
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "toor");
+				
+		// 2. Create a statement
+			Statement myStm = myConn.createStatement();
+		// 3. Execute sql query
+			ResultSet myRs = myStm.executeQuery("select * from account ");
+		// 4. Process the result set
+			
+			String test1 = "asmir";
+			String test2 = "1600";
+			
+			
+			while(myRs.next()) {
+				System.out.println(myRs.getString("username") + myRs.getString("balance"));
+				
+				if (myRs.getString("username").equals(test1) && myRs.getString("balance").equals(test2)) {
+				System.out.println("Confirmed access!!");
+			}
+			
+			
+				
+			}
+			
+		}
+		
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+		
+		
+
+	}
+
+}
